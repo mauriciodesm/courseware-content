6488ed39bd98ce2999bc0014b9a400bd585636b5
Mauricio Maciel <mauriciomaciel@id.uff.br>
2022-09-29 14:32:00 -0300

    Extension + Model + Assets

---
+++ ViewCode/Music App/Music App/Extension/NSTextAttachment + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  NSTextAttachment + Extension.swift
+//  AppMusicBF
+//
+//  Created by Caio on 06/10/21.
+//
+
+import Foundation
+import UIKit
+
+extension NSTextAttachment {
+    func setImageHeight(height: CGFloat) {
+        guard let image = image else { return }
+        let ratio = image.size.width / image.size.height
+        
+        bounds = CGRect(x: bounds.origin.x, y: bounds.origin.y, width: ratio * height, height: height)
+    }
+}
---
+++ ViewCode/Music App/Music App/Extension/UIView + Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIView.swift
+//  AppMusicBF
+//
+//  Created by Caio on 06/10/21.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func pin(to superView: UIView){
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        self.leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        self.bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        self.trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+    }
+    
+}
+
+
