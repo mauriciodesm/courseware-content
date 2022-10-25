cf1a34fb7d8b2b6181dffdb4a9cfed61db82d970
Mauricio Maciel <mauriciomaciel@id.uff.br>
2022-10-24 22:36:26 -0300

    Topico 4 - PUT e DELETE

--- Alura/Kotlin/Kotlin+Spring_Boot/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ Alura/Kotlin/Kotlin+Spring_Boot/forum/src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,14 +1,19 @@
 package br.com.alura.forum.controller
 
+import br.com.alura.forum.DTO.AtualizacaoTopicoForm
 import br.com.alura.forum.DTO.NovoTopicoForm
 import br.com.alura.forum.DTO.TopicoView
 import br.com.alura.forum.service.TopicoService
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.util.UriComponentsBuilder
 import javax.validation.Valid
 
 @RestController
@@ -16,16 +21,30 @@ import javax.validation.Valid
 class TopicoController(private val service: TopicoService) {
 
     @GetMapping
-    fun listar(): List<TopicoView> {
+    fun listar(): List<TopicoView>{
         return service.listar()
     }
 
     @GetMapping("/{id}")
-    fun buscarPorId(@PathVariable id:Long): TopicoView{
+    fun buscarPorId(@PathVariable id:Long): TopicoView {
         return service.buscarPorId(id)
     }
     @PostMapping
-    fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
-        return service.cadastrar(dto)
+    fun cadastrar(uriBuilder:UriComponentsBuilder,
+        @RequestBody @Valid form: NovoTopicoForm): ResponseEntity<TopicoView>{
+        val topicoView = service.cadastrar(form)
+        val uri = uriBuilder.path("/topicos/${topicoView.id}").build().toUri()
+        return  ResponseEntity.created(uri).body(topicoView)
+    }
+
+    @PutMapping
+    fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm): ResponseEntity<TopicoView>{
+        val topicoView= service.atualizar(form)
+        return ResponseEntity.ok(topicoView)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deletar (@PathVariable id: Long) {
+        service.deletar(id)
     }
 }
--- Alura/Kotlin/Kotlin+Spring_Boot/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ Alura/Kotlin/Kotlin+Spring_Boot/forum/src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.DTO.AtualizacaoTopicoForm
 import br.com.alura.forum.DTO.NovoTopicoForm
 import br.com.alura.forum.DTO.TopicoView
 import br.com.alura.forum.mapper.TopicoFormMapper
@@ -29,9 +30,35 @@ class TopicoService(
         return topicoViewMapper.map(topico)
     }
 
-    fun cadastrar(form: NovoTopicoForm) {
+    fun cadastrar(form: NovoTopicoForm): TopicoView {
         val topico = topicoFormMapper.map(form)
         topico.id = topicos.size.toLong() + 1
         topicos = topicos.plus(topico)
+        return topicoViewMapper.map(topico)
+    }
+
+    fun atualizar(form: AtualizacaoTopicoForm): TopicoView {
+        val topico =  topicos.stream().filter { t ->
+            t.id == form.id
+        }.findFirst().get()
+        val topicoAtualizado = Topico(
+            id = form.id,
+            titulo = form.titulo,
+            mensagem = form.mensagem,
+            autor = topico.autor,
+            curso = topico.curso,
+            respostas = topico.respostas,
+            status = topico.status,
+            dataCriacao = topico.dataCriacao
+        )
+        topicos = topicos.minus(topico).plus(topicoAtualizado)
+        return topicoViewMapper.map(topicoAtualizado)
+    }
+
+    fun deletar(id: Long) {
+        val topico =  topicos.stream().filter { t ->
+            t.id == id
+        }.findFirst().get()
+        topicos = topicos.minus(topico)
     }
 }
